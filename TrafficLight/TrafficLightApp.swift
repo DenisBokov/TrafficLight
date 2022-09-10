aec3df4aa4c775a60a4fd47b7f9e7e8c17110a4c
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-09-10 13:27:50 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Denis Bokov on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
